18c11f6e53504d62a0b0611ba6c26b4b3b433e6e
xiext-hub <76427756+xiext-hub@users.noreply.github.com>
2022-12-08 14:54:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to China"/>
+    <HelloWorld msg="Welcome to 4everland"/>
   </div>
 </template>
 
