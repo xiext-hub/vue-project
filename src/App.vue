34154cf8a349469246048004cc5548e93ca5f7d6
Sweet417 <104195743+Sweet417@users.noreply.github.com>
2023-06-13 17:18:40 +0800

    Update App.vue
    4everland4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to 4everland3"/>
+    <HelloWorld msg="Welcome to 4everland4"/>
   </div>
 </template>
 
