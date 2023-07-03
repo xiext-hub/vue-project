d8dc006d60f49baf754d20d9b3e896fcd7e254b5
Sweet417 <104195743+Sweet417@users.noreply.github.com>
2023-07-03 14:16:47 +0800

    Update App.vue
    Update App.vue 02

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to China-01"/>
+    <HelloWorld msg="Welcome to China-02"/>
   </div>
 </template>
 
