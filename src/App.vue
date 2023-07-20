045d84138f25aae62e104b7d67eb9e2314f2360b
Sweet417 <1075219353@qq.com>
2023-07-20 11:29:28 +0800

    提交了一些备注信息

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to China-03 -chang  an  san   wan   li "/>
+    <HelloWorld msg="Welcome to China-international"/>
   </div>
 </template>
 
